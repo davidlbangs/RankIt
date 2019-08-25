b5e4d22c9abb2230b2914d44b15ac11a0fa4d3f7
iambateman <stephen@bate-man.com>
2019-08-25 12:22:50 -0400

    attempting prerender

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,6 +2,7 @@ import * as functions from 'firebase-functions';
 const admin = require('firebase-admin');
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
 // SEO IMPORTS
 // const express = require('express');
@@ -25,8 +26,14 @@ export const syncPoll = vote.syncPoll;
 export const closePoll = poll.closePolls;
 
 export const helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hola... from Firebase!");
+ response.send("Hola:( from Firebase!");
+});
+
+export const corsServer = express();
+corsServer.use(cors({origin: true}));
+corsServer.get('*', (request, response) => {
+  response.send('Hello from Express on Firebase with CORS!');
 });
 
 
-app.use(require('prerender-node').set('prerenderToken', 'Dmq9oEPFIqRzSjxKGFhf'));
+export const prerender = app.use(require('prerender-node').set('prerenderToken', 'Dmq9oEPFIqRzSjxKGFhf'));
