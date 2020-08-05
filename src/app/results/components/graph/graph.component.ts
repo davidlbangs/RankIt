e8de5da6994bf4b4f55e61afe6fe481b19d9f980
Ralph Kuepper <ralph.kuepper@skelpo.com>
2020-08-05 15:51:35 -0400

    threshold line

--- src/app/results/components/graph/graph.component.ts
+++ src/app/results/components/graph/graph.component.ts
@@ -38,7 +38,7 @@ export class GraphComponent implements OnInit {
     let round = this.round;
 
     let sorted = this.all_choices;
-
+   // console.log("choices: ", this.results.rounds);
     // hardcoding to round 1, to avoid resorting after they start going through results.
     let choicesWithVotes = this.results.rounds[1];
 
@@ -47,13 +47,13 @@ export class GraphComponent implements OnInit {
     
 
     // move items that got zero votes to the bottom
-    for(let x of sorted) {
+    /*for(let x of sorted) {
       if(!(x in choicesWithVotes)){
         sorted.push(sorted.splice(sorted.indexOf(x), 1)[0]);
       }
-    }
+    }*/
     
-    sorted = this.all_choices.sort((a, b) => (choicesWithVotes[a] > choicesWithVotes[b]) ? -1 : 1);
+    //sorted = this.all_choices.sort((a, b) => (choicesWithVotes[a] > choicesWithVotes[b]) ? -1 : 1);
 
     // console.log('running sort', sorted, this.results.rounds[1], this.all_choices);
     return sorted;
@@ -90,7 +90,9 @@ export class GraphComponent implements OnInit {
                                              this.threshold,
                                              this.total_votes);
   }
-
+  getThreshold() {
+    return (Math.round(this.threshold/this.total_votes*10000)/100)+"%";
+  }
   getCount(round: number, choice:Choice) {
     return this.resultsService.getCount(round, 
                                              choice, 
